1a00ceb265c1f3134101f2c041d5bfce31d486b9
unknown <jtjackson325@gmail.com>
2021-08-24 15:10:33 -0600

    got the majority of the stuff for the project turned in

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -8,5 +8,31 @@ request.onupgradeneeded = function(event) {
     const db = event.target.result;
     // create an object store (table) called `new.pizza`, set it to have an auto incrementing primary key of sorts
     db.createdObjectStore('new_pizza', { autoIncrement: true });
+}
 
+// upon a successful connection
+request.onsuccess = function(event) {
+    // when db is succesfully created with its object store (from onupgraded event above) or simply established a connection, save reference to db in global variable
+    db = event.target.resultl;
+
+    // check if app is online, if yes run uploadPizza() function to send all local db data to api
+    if (navigator.onLine) {
+        // uploadPizza();
+    }
+};
+
+request.onerror = function(event) {
+    // log error here 
+    console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions
+    const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+    // access object store for `new_pizza`
+    const pizzaObjectStore = transaction.objectStore('new_pizza');
+
+    // add record to your store with add method
+    pizzaObjectStore.add(record);
 }
